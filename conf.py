caba4cf081acd47c5f6015219ec7b622ad6b738a
lshpaner <lshpaner@ucla.edu>
2024-07-09 11:43:33 -0700

    testing sphinx docs on readthedocs theme

--- conf.py
+++ conf.py
@@ -33,3 +33,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_baseurl = 'https://uclamii.github.io/model_tuner/docs/'
