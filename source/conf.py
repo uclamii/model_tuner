36460d4d0c15c95c9dbbb01fd238f81c7c592489
lshpaner <lshpaner@ucla.edu>
2024-07-09 12:28:05 -0700

    created front page matter of documentation

--- source/conf.py
+++ source/conf.py
@@ -17,11 +17,23 @@ release = '0.0.8a0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
+    "sphinx.ext.autodoc",
+    "sphinx_copybutton",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
 ]
+
+extensions.append("sphinx.ext.intersphinx")
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+}
+
+# copybutton_prompt_text = r">|\$ "
+# copybutton_prompt_is_regexp = True
+
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -36,3 +48,4 @@ html_static_path = ['_static']
 
 # If your documentation is served from a subdirectory, set this to the subdirectory path
 html_baseurl = 'https://uclamii.github.io/model_tuner/'
+html_show_sourcelink = False
