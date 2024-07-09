613a68c5f0affbf16f8d4d2a7369b52880855773
lshpaner <lshpaner@ucla.edu>
2024-07-09 11:21:49 -0700

    testing sphinx docs on readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,12 @@ release = '0.0.8a0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
