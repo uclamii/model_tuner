cd9a1ab4cf36d470fb5379ad1b00a4a51d80b694
lshpaner <lshpaner@mednet.ucla.edu>
2024-11-21 17:30:08 -0800

    removed resolved comments from 04_07_24

--- src/model_tuner/model_tuner_utils.py
+++ src/model_tuner/model_tuner_utils.py
@@ -115,7 +115,7 @@ class Model:
         feature_selection=False,
         class_labels=None,
         multi_label=False,
-        calibration_method="sigmoid",  # 04_27_24 --> added calibration method
+        calibration_method="sigmoid",  
         custom_scorer=[],
         bayesian=False,
     ):
@@ -134,7 +134,7 @@ class Model:
         self.model_type = model_type
         self.multi_label = multi_label
         self.calibration_method = (
-            calibration_method  # 04_27_24 --> added calibration method
+            calibration_method 
         )
         self.imbalance_sampler = imbalance_sampler
 
