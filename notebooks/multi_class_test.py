43b22e51322224692e4dfcfc9e55ffa168c21f55
elemets <afunnell@mednet.ucla.edu>
2024-11-26 17:02:29 -0800

    Adding multi class test script

---
+++ notebooks/multi_class_test.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+
+from sklearn.datasets import make_classification
+
+from sklearn.datasets import load_breast_cancer
+
+from model_tuner.model_tuner_utils import Model
+from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.datasets import load_iris
+
+data = load_iris()
+X = data.data
+y = data.target
+
+X = pd.DataFrame(X)
+y = pd.DataFrame(y)
+
+
+estimator = RandomForestClassifier()
+
+estimator_name = "rf_mc"
+xgbearly = False
+
+tuned_parameters = {
+    f"{estimator_name}__max_depth": [3, 10, 15],
+    f"{estimator_name}__n_estimators": [5, 10, 15, 20],
+}
+
+kfold = False
+calibrate = False
+
+
+model = Model(
+    name="Random Forest Multi Class",
+    model_type="classification",
+    estimator_name=estimator_name,
+    pipeline_steps=[("impute", SimpleImputer())],
+    calibrate=calibrate,
+    estimator=estimator,
+    kfold=kfold,
+    stratify_y=True,
+    grid=tuned_parameters,
+    multi_label=True,
+    randomized_grid=False,
+    n_iter=4,
+    scoring=["roc_auc_ovr"],
+    n_jobs=-2,
+    random_state=42,
+    class_labels=["1", "2", "3"],
+)
+
+
+model.grid_search_param_tuning(X, y)
+
+X_train, y_train = model.get_train_data(X, y)
+X_test, y_test = model.get_test_data(X, y)
+X_valid, y_valid = model.get_valid_data(X, y)
+
+model.fit(X_train, y_train, validation_data=[X_valid, y_valid])
+
+print("Validation Metrics")
+model.return_metrics(X_valid, y_valid)
+
+y_prob = model.predict_proba(X_test)
+print("Test Metrics")
+model.return_metrics(X_test, y_test)
+
+# y_prob = model.predict_proba(X_test)[:, 1]
+
+# ### F1 Weighted
+# y_pred = model.predict(X_test, optimal_threshold=True)
