3c9b5874946a64c387abd5ed62eb7c479b495628
lshpaner <lshpaner@ucla.edu>
2025-02-13 10:43:08 -0800

    Removing model selection as a documented library feature; Adding training and tuning to short desc.

--- src/model_tuner/__init__.py
+++ src/model_tuner/__init__.py
@@ -12,8 +12,8 @@ The `model_tuner` library is a versatile and powerful tool designed to
 facilitate the training, evaluation, and tuning of machine learning models. 
 It supports various functionalities such as handling imbalanced data, applying 
 different scaling and imputation techniques, calibrating models, and conducting 
-cross-validation. This library is particularly useful for model selection, 
-hyperparameter tuning, and ensuring optimal performance across different metrics.
+cross-validation. This library is particularly useful for hyperparameter tuning
+and ensuring optimal performance across different metrics.
 
 PyPI: https://pypi.org/project/model-tuner/
 Documentation: https://uclamii.github.io/model_tuner/
